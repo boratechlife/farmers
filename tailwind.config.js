64506e1f5d511529b7d7ba26c4db49fdc18d6b85
boratechlife <boratechlife@gmail.com>
2023-12-21 21:33:47 +0300

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary:"#359744",
+        secondary: "#8AD23B"
+      }
+    },
+  },
+  plugins: [],
+}
