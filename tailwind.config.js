8a2104fa392c638c4134df1c60b9a03aa02e517f
Franklin Shera <fshera96@gmail.com>
2023-12-24 00:12:58 +0300

    top farms

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,22 @@ module.exports = {
       colors: {
         primary: "#359744",
         secondary: "#8AD23B",
+        tertiary: "#161E54",
         light: "#fafafa",
+        "blue-gray": "#475467",
+
+        dark: "#1c1c1c",
         success: {
           1: "#5B9051",
           2: "#E9FEEE",
+          3: "#334C36",
         },
         gray: {
           1: "#8C8C8C",
+          2: "#D0D0D0",
+          3: "#B2B2B2",
+          4: "#F1F1F1",
+          5: "#CBCDCC",
         },
       },
     },
