dcc48d2e2243c699fa57d45620269e437a2fe27d
Franklin Shera <fshera96@gmail.com>
2023-12-23 22:03:45 +0300

    add sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
+/** @type {import("tailwindcss/defaultTheme")} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", ...defaultTheme.fontFamily.serif],
+      },
       colors: {
-        primary:"#359744",
-        secondary: "#8AD23B"
-      }
+        primary: "#359744",
+        secondary: "#8AD23B",
+        light: "#fafafa",
+        success: {
+          1: "#5B9051",
+          2: "#E9FEEE",
+        },
+        gray: {
+          1: "#8C8C8C",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
